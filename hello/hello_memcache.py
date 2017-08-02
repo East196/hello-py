2ba3cf03145e7e236ded5971f6b384312a454aca
east196 <2901180515@qq.com>
2017-08-02 17:03:05 +0800

    add memcache hello

---
+++ hello/hello_memcache.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pymemcache.client.base import Client
+
+client = Client(('localhost', 11211))
+client.set('some_key', 'some_value')
+result = client.get('some_key')
+print result
