0713e7d3d2eb675d0c63fd53e5a268f00c61d177
east196 <2901180515@qq.com>
2017-11-19 23:05:20 +0800

    tool

---
+++ hello/test_tool.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import yaml
+from tool import *
+
+
+def test_search_path():
+    yo = """
+    a: 
+      b: 
+        c: 焦飞
+    d: 太上化龙决
+    e: 解
+    """
+
+    o = yaml.load(yo)
+    assert "e" == search_path(o, u"解")
+    assert "a.b.c" == search_path(o, u"焦飞")
+
+
+def test_leaf_path():
+    yo = """
+    a: 
+      b: 
+        c: 焦飞
+    d: 太上化龙决
+    e: 解
+    """
+
+    o = yaml.load(yo)
+    for k, v in leaf_path(o):
+        print k, v
