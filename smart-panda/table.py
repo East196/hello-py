af3e045a048cb24f1951f4cca451410b118b5e84
east196 <2901180515@qq.com>
2017-12-01 21:44:22 +0800

    panda milk

---
+++ smart-panda/table.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+import time
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv("food.csv")
+df["day"] = df["time"].apply(lambda time: time.split(" ")[0])
+df2 = df[df.type == "milk"].groupby(by=["day"])["size"].agg({"size": np.sum}).reindex()
+
+df2.plot()
+# plt.show()
+# raw_input()
