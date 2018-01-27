58017e5156bf9b373cb44ce06f55a929e9e44a3c
east196 <2901180515@qq.com>
2018-01-27 22:29:11 +0800

    ^ winpcap pip

--- grey/hello_winpcap.py
+++ grey/hello_winpcap.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# pip install pypiwin32 winpcapy
+
 from winpcapy import WinPcapDevices
+
 # Return a list of all the devices detected on the machine
 print(WinPcapDevices.list_devices())
 
