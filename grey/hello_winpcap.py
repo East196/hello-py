f055112392c9f5a9db854e83d65f7b831e648d00
east196 <2901180515@qq.com>
2018-01-27 22:27:13 +0800

    + winpcap

---
+++ grey/hello_winpcap.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from winpcapy import WinPcapDevices
+# Return a list of all the devices detected on the machine
+print(WinPcapDevices.list_devices())
+
+from winpcapy import WinPcapUtils
+
+print(WinPcapUtils.capture_on_and_print("*Microsoft*"))
