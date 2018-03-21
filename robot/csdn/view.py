1038a50e061453213402e8523c8b9e799d7611d0
east196 <2901180515@qq.com>
2018-03-21 23:46:38 +0800

    + fastview csdn

--- robot/csdn/view.py
+++ robot/csdn/view.py
@@ -7,23 +7,32 @@ import random
 from selenium import webdriver
 import time
 
-browser = webdriver.Firefox()
-browser.maximize_window()
-browser.get("http://blog.csdn.net/east196")
-time.sleep(5)
 
-urls = []
-html_as = browser.find_elements_by_css_selector("li.blog-unit a")
-for html_a in html_as:
-    urls.append(html_a.get_attribute("href"))
+def view(browser, blog):
+    browser.maximize_window()
+    browser.get(blog)
+    time.sleep(5)
 
-print urls
+    urls = []
+    html_as = browser.find_elements_by_css_selector("li.blog-unit a")
+    for html_a in html_as:
+        urls.append(html_a.get_attribute("href"))
 
-for html in urls:
-    try:
-        browser.get(html)
-        time.sleep(random.randint(3, 5))
-    except Exception, e:
-        print e
+    print urls
 
-browser.close()
+    for html in urls:
+        try:
+            browser.get(html)
+            time.sleep(random.randint(3, 5))
+        except Exception, e:
+            print e
+
+
+browsers = [webdriver.Firefox(), webdriver.PhantomJS()]
+# browsers = [webdriver.Ie()]
+blogs = ["https://blog.csdn.net/q809198545", "http://blog.csdn.net/east196"]
+
+for browser in browsers:
+    for blog in blogs:
+        view(browser, blog)
+    browser.close()
