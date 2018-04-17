d3c2736794b6533b7aca0631001f310f3c22325a
east196 <2901180515@qq.com>
2018-04-17 23:57:17 +0800

    + itchat

---
+++ hello/hello_itchat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import itchat
+
+
+@itchat.msg_register(itchat.content.TEXT)
+def print_content(msg):
+    print(msg)
+
+    itchat.send('greeting, filehelper!', "filehelper")
+    name = itchat.search_friends(name=u'谁啊')[0]
+    print name
+    name.send("good")
+
+
+itchat.auto_login(hotReload=True)
+itchat.run()
+
+## and 基于itchat的wxpy
